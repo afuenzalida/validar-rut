f7092cb7d51d80aef54c5a6725b08aff082a2466
Gabriel P??rez <gabriel@garox.org>
2022-11-04 17:21:18 -0300

    fix generateRandomRUT on locales that use comma as separator

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function clearRUT(rut) {
 function generateRandomRUT(amount = 1, dots = false, hyphen = false) {
   const generatedRUTs = [...Array(amount).keys()].map(() => {
     const rut = Math.floor(1000000 + Math.random() * 30000000)
-    return `${dots ? rut.toLocaleString() : rut}${hyphen ? '-' : ''}${getCheckDigit(rut)}`
+    return `${dots ? rut.toLocaleString("es-CL") : rut}${hyphen ? '-' : ''}${getCheckDigit(rut)}`
   })
 
   return amount === 1 ? generatedRUTs[0] : generatedRUTs
